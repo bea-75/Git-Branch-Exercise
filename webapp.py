e051aea6d55709fc9ab0a056d8606598d9082714
bea-75 <beatrizesouthern75@gmail.com>
2023-05-02 13:53:36 -0700

    Displays randomID

--- webapp.py
+++ webapp.py
@@ -1,10 +1,12 @@
 from datetime import date
+import uuid
 
 today = date.today()
+name = 'Bea'
+date = today.strftime("%m/%d/%y")
+randomID = uuid.uuid4()
 
 print('hello world')
 print('my name is ' + name)
 print("today's date is " + date)
-
-name = 'Bea'
-date = today.strftime("%m/%d/%y")
+print("your id is " + str(randomID))
