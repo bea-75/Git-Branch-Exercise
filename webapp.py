801b139ef3a5c636a9cdfcd412634834f9ecaa51
bea-75 <129797236+bea-75@users.noreply.github.com>
2023-05-02 13:31:37 -0700

    fix variable bug

--- webapp.py
+++ webapp.py
@@ -1,10 +1,9 @@
 from datetime import date
 
 today = date.today()
+name = 'Bea'
+date = today.strftime("%m/%d/%y")
 
 print('hello world')
 print('my name is ' + name)
 print("today's date is " + date)
-
-name = 'Bea'
-date = today.strftime("%m/%d/%y")
