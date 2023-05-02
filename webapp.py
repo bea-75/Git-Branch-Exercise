a2ad639b5cfa74cc3b3d9cc2624d5c58350e5b42
bea-75 <beatrizesouthern75@gmail.com>
2023-05-02 13:26:35 -0700

    Displays todays date

--- webapp.py
+++ webapp.py
@@ -1,10 +1,6 @@
-from datetime import date
-
-today = date.today()
-
 print('hello world')
 print('my name is ' + name)
 print("today's date is " + date)
 
 name = 'Bea'
-date = today.strftime("%m/%d/%y")
+date = '5/2/23'
